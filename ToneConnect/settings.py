0f4744fc017653a1d1553e40e2c944ecbf08a70c
trv449 <donghyun0215@naver.com>
2024-11-13 12:48:20 +0900

    fix : cors

--- ToneConnect/settings.py
+++ ToneConnect/settings.py
@@ -41,18 +41,19 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
     "translate",
     "MailRecommend",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
